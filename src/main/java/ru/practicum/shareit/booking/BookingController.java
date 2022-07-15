abb1a0c4370601b0563b5b43c1aa86120986c0a9
Rakhman <kichibekovrahman@yandex.ru>
2022-07-15 13:10:51 +0300

    todo исправлен

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -2,8 +2,10 @@ package ru.practicum.shareit.booking;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 /**
  * // TODO .
+ * Класс будет реализован в следующих спринтах
  */
 @RestController
 @RequestMapping(path = "/bookings")
