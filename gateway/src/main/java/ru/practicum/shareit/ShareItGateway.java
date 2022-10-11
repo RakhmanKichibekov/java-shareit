6dd570ab55a54b983ed27a72a5999e59b8da0e67
RakhmanKichibekov <kichibekovrahman@yandex.ru>
2022-10-11 13:45:51 +0300

    add configuration and mainClass

--- gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
+++ gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
@@ -1,4 +1,4 @@
-package ru.practicum.shareit;
+package java.ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
